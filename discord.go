aa8db9ce4fdce1722302657afc5687f78453a5ef
Xnopyt <billy@xnopyt.info>
2019-12-30 13:18:07 +0000

    Members now populate for private channels

--- discord.go
+++ discord.go
@@ -323,6 +323,12 @@ func (m *mainBind) LoadDMChannel(id string) {
 	wv.Dispatch(func() {
 		wv.Eval(fmt.Sprintf(`selectdmchannel(%q, %q);`, id, html.EscapeString(user.Username)))
 		wv.Eval(`document.getElementById("mainbox").style.visibility = "hidden";`)
+		wv.Eval(`resetmembers();`)
+		wv.Eval(fmt.Sprintf(`addmember(%q, %q)`, ses.State.User.Username, ses.State.User.AvatarURL("128")))
+		for _, v := range channel.Recipients {
+			wv.Eval(fmt.Sprintf(`addmember(%q, %q)`, v.Username, v.AvatarURL("128")))
+		}
+		wv.Eval(fmt.Sprintf(`setmembercount("%d");`, len(channel.Recipients)+1))
 		msgs, err := ses.ChannelMessages(channel.ID, 18, "", "", "")
 		if err != nil {
 			log.Println(err)
